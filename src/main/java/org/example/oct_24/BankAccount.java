b1b513abee7714fac8565e6961890846bd7cbba2
Ester Gherbezza <ester.gherbezza@gmail.com>
2024-10-21 08:50:48 +0200

    first

---
+++ src/main/java/org/example/oct_24/BankAccount.java
@@ -0,0 +1,18 @@
+package org.example.oct_24;
+
+public class BankAccount {
+    private double balance;
+    public BankAccount(double balance){
+        this.balance = balance;
+    }
+    public void deposit(double amount){
+        this.balance += amount;
+    }
+    public void withdraw(double amount){
+        this.balance -= amount;
+    }
+
+    public double getBalance(){
+        return this.balance;
+    }
+}
---
+++ src/main/java/org/example/oct_24/BankAccountTester.java
@@ -0,0 +1,9 @@
+package org.example.oct_24;
+
+public class BankAccountTester {
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(100.40);
+
+    }
+}
